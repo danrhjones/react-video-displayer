addb3d04d2834231061a199eba1d1e211eee3e21
Dan Jones <dan.rh.jones@gmail.com>
2019-12-03 23:33:31 +0000

    video will now update when a new search term is selected

--- src/component/App.js
+++ src/component/App.js
@@ -15,7 +15,10 @@ export default class App extends React.Component {
         q: term
       }
     });
-    this.setState({videos: response.data.items});
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = (video) => {
